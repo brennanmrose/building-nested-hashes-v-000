b82a6b1841862b316511ff38d7b39a2b1efad52f
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-26 12:10:12 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,7 +6,7 @@ def third_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {:name => "Benvolio", :age => "17", :attitude => "worried"},
-        {:name => "Nurse", :age => "44", :attitude => "worried"}
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
         ]
    }, 
    :capulet => {
